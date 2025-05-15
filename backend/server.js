3bbd157020e80f5a7f78cade8ffe9c545aacc06f
Alx1307 <ekhmara75@yandex.ru>
2025-05-15 23:00:11 +0300

    добавлено редактирование профиля

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,11 @@ const database = require('./src/config/database');
 const sequelize = database.sequelize;
 const YAML = require('yamljs');
 const swaggerUI = require('swagger-ui-express');
+const swaggerDoc = YAML.load('./src/docs/spec.yaml');
 require('dotenv').config();
-
 
 const librarianRoutes = require('./src/routes/librarianRoutes');
 const bookRoutes = require('./src/routes/bookRoutes');
-
-const swaggerDoc = YAML.load('./src/docs/spec.yaml');
 
 const app = express();
 app.use(express.json());
@@ -23,5 +21,5 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 const PORT = 8080;
 app.listen(PORT, async () => {
-    console.log(`Сервер запущен на порту http://localhost:${PORT}`);
+    console.log(`Сервер запущен на порту http://localhost:${PORT}/api-docs`);
 });
