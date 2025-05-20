462f022855561581044baf732a2d1016e480b41e
Alx1307 <ekhmara75@yandex.ru>
2025-05-20 13:35:02 +0300

    написан метод получения информации о выдаче

--- backend/src/controllers/issueController.js
+++ backend/src/controllers/issueController.js
@@ -5,8 +5,10 @@ const Book = require('../models/Book');
 const Reader = require('../models/Reader');
 
 class IssueController {
-    constructor(IssueModel) {
-        this.Issue = IssueModel
+    constructor(IssueModel, BookModel, ReaderModel) {
+        this.Issue = IssueModel,
+        this.Book = BookModel,
+        this.Reader = ReaderModel
     }
 
     async addIssue(req, res) {
@@ -45,6 +47,47 @@ class IssueController {
             });
     
             return res.status(201).send('Книга успешно выдана.');
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).send(err.message);
+        }
+    }
+
+    async getIssueData(req, res) {
+        try {
+            if (req.userData.role !== 'Библиотекарь') {
+                return res.status(403).send('Доступ запрещён.');
+            }
+    
+            const { issue_id } = req.params;
+    
+            if (!issue_id) {
+                return res.status(400).send('Некорректный id.');
+            }
+    
+            const issue = await this.Issue.findByPk(issue_id, {
+                attributes: ['issue_id', 'book_id', 'reader_id'],
+            });
+    
+            if (!issue) {
+                return res.status(404).send('Нет такой выдачи');
+            }
+    
+            const book = await this.Book.findByPk(issue.book_id, {
+                attributes: ['code']
+            });
+    
+            const reader = await this.Reader.findByPk(issue.reader_id, {
+                attributes: ['card_number']
+            });
+    
+            const issueData = {
+                ...issue.toJSON(),
+                BookCode: book.code,
+                ReaderCardNumber: reader.card_number
+            };
+    
+            return res.status(200).json(issueData);
         } catch (err) {
             console.error(err.message);
             return res.status(500).send(err.message);
--- backend/src/routes/issueRoutes.js
+++ backend/src/routes/issueRoutes.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const IssueController = require('../controllers/issueController');
 const Issue = require('../models/Issue');
+const Book = require('../models/Book');
+const Reader = require('../models/Reader');
 const authMiddleware = require('../middleware/authMiddleware');
 
-const controller = new IssueController(Issue);
+const controller = new IssueController(Issue, Book, Reader);
 
 router.post('/add', authMiddleware, controller.addIssue.bind(controller));
+
+router.get('/get/:issue_id', authMiddleware, controller.getIssueData.bind(controller));
 
 module.exports = router;
